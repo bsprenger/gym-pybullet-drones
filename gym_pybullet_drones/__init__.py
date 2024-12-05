9ca9662d91eeb70f98478f3d328cfd399522b6d3
bsprenger <ben.sprenger@rogers.com>
2024-12-05 14:11:09 +0100

    chore: rename gym envs to match lerobot task convention

--- gym_pybullet_drones/__init__.py
+++ gym_pybullet_drones/__init__.py
@@ -1,21 +1,21 @@
 from gymnasium.envs.registration import register
 
 register(
-    id='ctrl-aviary-v0',
+    id='gym_pybullet_drones/ctrl-aviary-v0',
     entry_point='gym_pybullet_drones.envs:CtrlAviary',
 )
 
 register(
-    id='velocity-aviary-v0',
+    id='gym_pybullet_drones/velocity-aviary-v0',
     entry_point='gym_pybullet_drones.envs:VelocityAviary',
 )
 
 register(
-    id='hover-aviary-v0',
+    id='gym_pybullet_drones/hover-aviary-v0',
     entry_point='gym_pybullet_drones.envs:HoverAviary',
 )
 
 register(
-    id='multihover-aviary-v0',
+    id='gym_pybullet_drones/multihover-aviary-v0',
     entry_point='gym_pybullet_drones.envs:MultiHoverAviary',
 )
